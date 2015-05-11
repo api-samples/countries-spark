28b55e5c5f2add74d68781d335a3543c51712ccd
Mikael Gueck <gumi@iki.fi>
2015-05-11 18:54:34 +0300

    initial

---
+++ src/main/java/io/mikael/countries/SparkCountries.java
@@ -0,0 +1,41 @@
+package io.mikael.countries;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toMap;
+import static spark.Spark.get;
+
+public class SparkCountries {
+
+    private final static Map<String, Country> COUNTRIES;
+
+    private final static ObjectMapper JSON;
+
+    static {
+        JSON = new ObjectMapper();
+        JSON.enable(SerializationFeature.INDENT_OUTPUT);
+        try (final InputStream is = SparkCountries.class.getClassLoader().getResourceAsStream("countries.json")) {
+            final List<Country> input = JSON.readValue(is, new TypeReference<List<Country>>() { });
+            COUNTRIES = input.stream().collect(
+                    toMap(c -> c.cca2, Function.identity(), (a, b) -> a));
+        } catch (final IOException e) {
+            throw new RuntimeException("unable to parse countries", e);
+        }
+    }
+
+    public static void main(final String ... args) throws Exception {
+        get("/countries/:cca2", (req, res) -> {
+            res.type("text/json");
+            return COUNTRIES.get(req.params(":cca2"));
+        }, JSON::writeValueAsString);
+    }
+
+}
